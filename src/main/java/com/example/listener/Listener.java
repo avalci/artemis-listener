8bbbadc2c4098be8a209c04e29007e39ee30bb8c
Avalci Suzano <avalci@gmail.com>
2021-10-29 00:26:55 -0300

    receiving objects

--- src/main/java/com/example/listener/Listener.java
+++ src/main/java/com/example/listener/Listener.java
@@ -5,13 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
+import com.example.dto.EntityDTO;
+
 @Service
 public class Listener {
 
 	private Logger logger = LoggerFactory.getLogger(Listener.class);
 	
-	@JmsListener(destination = "${jms.queue}")
-	public void receiveMessage(String message) {
-		logger.info("Received message: " + message);
+	@JmsListener(destination = "${jms.queue01}")
+	public void receiveText(String text) {
+		logger.info("Received text: " + text);
 	}
+	
+	@JmsListener(destination = "${jms.queue02}", containerFactory = "myFactory")
+	public void receiveObject(EntityDTO object) {
+		logger.info("Received object: " + object);
+	}
+
 }
